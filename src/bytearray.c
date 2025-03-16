e1110b56f3a76eb2502aba38fb8a89eba21b3ffa
8dcc <8dcc.git@gmail.com>
2025-03-16 22:53:48 +0100

    Print offset in 'bytearray_print'
    * Add 'bytearray_print_at', turn 'bytearray_print' into a wrapper.

--- src/bytearray.c
+++ src/bytearray.c
@@ -7,34 +7,39 @@
 #include "include/util.h"
 
 /*
- * Word size in bytes. A space will be printed between words. Set to zero to
- * disable.
+ * TODO: Perhaps change 'fill_column' argument to 'words_per_line'.
  */
-#define WORD_SIZE 2
-
-/*
- * Maximum column used for printing the arrays. Set to zero to disable.
- */
-#define FILL_COLUMN 80
-
-void bytearray_print(FILE* fp, ByteArray* arr) {
-    const bool split_words = (WORD_SIZE >= 1);
-    const size_t word_size = (split_words) ? WORD_SIZE : 1;
+void bytearray_print_at(FILE* fp,
+                        const ByteArray* arr,
+                        size_t visual_offset,
+                        size_t word_size,
+                        size_t fill_column) {
+    const bool split_words = (word_size >= 1);
+    if (!split_words)
+        word_size = 1;
 
     const size_t total_word_chars =
       word_size * STRLEN("FF") + ((split_words) ? STRLEN(" ") : 0);
-    const size_t words_per_line = FILL_COLUMN / total_word_chars;
+    const size_t words_per_line =
+      (fill_column - STRLEN("0000: ")) / total_word_chars;
+
+    /* First offset */
+    fprintf(fp, "%04zX: ", visual_offset);
 
     for (size_t i = 0; i < arr->size; i++) {
+        /* Current byte */
         fprintf(fp, "%02X", ((uint8_t*)arr->data)[i]);
 
         const size_t bytes_printed = (i + 1);
-        if (bytes_printed % word_size == 0) { /* Just ended a word */
+        if (bytes_printed % word_size == 0) {
+            /* Just ended a word */
             const size_t words_printed = bytes_printed / word_size;
             if (words_per_line != 0 && words_printed % words_per_line == 0)
-                fputc('\n', fp);
+                fprintf(fp, "\n%04zX: ", visual_offset + i + 1);
             else if (split_words)
                 fputc(' ', fp);
         }
     }
+
+    /* TODO: Print final newline when needed. */
 }
--- src/include/bytearray.h
+++ src/include/bytearray.h
@@ -3,7 +3,7 @@
 #define BYTE_ARRAY_H_ 1
 
 #include <stddef.h>
-#include <stdio.h>  /* FILE */
+#include <stdio.h> /* FILE */
 
 /*
  * Generic array structure.
@@ -17,7 +17,30 @@ typedef struct {
 
 /*
  * Print the specified byte array to the specified file.
+ *
+ * The 'visual_offset' argument indicates the initial offset to be displayed
+ * when printing the file, but not the actual offset when reading the array.
+ *
+ * The 'word_size' argument indicates the number of bytes (not bits) to be
+ * grouped together by spaces. Set to zero to disable word grouping.
+ *
+ * The 'fill_column' argument indicates the maximum number of characters that a
+ * single line can use before switching to the next one. Set to zero to disable
+ * line wrapping.
  */
-void bytearray_print(FILE* fp, ByteArray* arr);
+void bytearray_print_at(FILE* fp,
+                        const ByteArray* arr,
+                        size_t visual_offset,
+                        size_t word_size,
+                        size_t fill_column);
+
+/*
+ * Simple wrapper for 'bytearray_print_at'.
+ */
+static inline void bytearray_print(FILE* fp, ByteArray* arr) {
+    const size_t default_word_size   = 2;
+    const size_t default_fill_column = 80;
+    bytearray_print_at(fp, arr, 0, default_word_size, default_fill_column);
+}
 
 #endif /* BYTE_ARRAY_H_ */
